ac9a26f83e8fd2abd5b8fd52caf42f2f0daad239
byronhopps <byron.hopps@gmail.com>
2017-02-06 22:09:48 -0600

    Change type of fullAdder IO to remove warnings

--- Lab 4/adders/ripple_adder.sv
+++ Lab 4/adders/ripple_adder.sv
@@ -32,8 +32,8 @@ module ripple_adder
 
 endmodule
 
-module fullAdder (input a, b, cin,
-                  output s, cout);
+module fullAdder (input logic a, b, cin,
+                  output logic s, cout);
 
     assign s = a^b^cin;
     assign cout = (a&b)|(b&cin)|(cin&a);
