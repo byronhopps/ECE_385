fc2a143c3c562f18f94c4dafa69230924945f6d5
byronhopps <byron.hopps@gmail.com>
2017-02-12 13:13:53 -0600

    Add module to determine next state

--- Lab 5/8bit_multiplier.sv
+++ Lab 5/8bit_multiplier.sv
@@ -15,4 +15,65 @@ module multiplier_8bit (input logic  Clk, Reset, ClearA_LoadB, Run,
                         output logic [6:0]  AhexL, AhexU, BhexL, BhexU);
 
 endmodule
+
+
+module stateSelector (input logic        Clk, Reset, ClearA_LoadB, Run, C, M,
+                      input state curState,
+                      output state nextState);
+
+//enum logic [3:0] {ready, reset, clrA_ldB, count, add, shift, done} curState, nextState;
+
+    // Assign outputs based on state
+    always_comb begin
+
+        // Default to staying in the current state
+        nextState = curState;
+
+        // Logic for switching state
+        case (curState)
+
+            // Ready state
+            ready: begin
+                if (Run == 1'b1)
+                    nextState = count;
+                else if (ClearA_LoadB == 1'b1)
+                    nextState = clrA_ldB;
+            end
+
+            // Reset state
+            reset: begin
+                if (Reset == 1'b0)
+                    nextState = ready;
+            end
+
+            // ClearA_LoadB state
+            clrA_ldB: nextState = ready;
+
+            // Count state
+            count: begin
+                case (M)
+                    1'b1: nextState = add;
+                    1'b0: nextState = shift;
+                endcase
+            end
+
+            // Add state
+            add: nextState = shift;
+
+            // Shift state
+            shift: begin
+                case (C)
+                    1'b1: nextState = done;
+                    1'b0: nextState = count;
+                endcase
+            end
+
+            // Done state
+            done: begin
+                if (Run == 1'b0)
+                    nextState = ready;
+            end
+        endcase
+    end
+
 endmodule
