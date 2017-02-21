7b796f1cfc699169f9b4bd2357834c67e710081a
byronhopps <byron.hopps@gmail.com>
2017-02-21 00:21:43 -0600

    Remove trailing whitespace in slc3.sv

--- Lab 6/slc3.sv
+++ Lab 6/slc3.sv
@@ -19,16 +19,16 @@
 
 
 module slc3(
-	input logic [15:0] S,
-	input logic	Clk, Reset, Run, Continue,
-	output logic [11:0] LED,
-	output logic [6:0] HEX0, HEX1, HEX2, HEX3,
-	output logic CE, UB, LB, OE, WE,
-	output logic [19:0] ADDR,
-	inout wire [15:0] Data //tristate buffers need to be of type wire
+    input logic [15:0] S,
+    input logic    Clk, Reset, Run, Continue,
+    output logic [11:0] LED,
+    output logic [6:0] HEX0, HEX1, HEX2, HEX3,
+    output logic CE, UB, LB, OE, WE,
+    output logic [19:0] ADDR,
+    inout wire [15:0] Data //tristate buffers need to be of type wire
 );
 
-// Declaration of push button active high signals	
+// Declaration of push button active high signals
 logic Reset_ah, Continue_ah, Run_ah;
 
 assign Reset_ah = ~Reset;
