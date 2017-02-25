7b39130563f2016a33bae7d791af740e88438efe
byronhopps <byron.hopps@gmail.com>
2017-02-25 10:39:43 -0600

    Added internal registers to testbench

--- Lab 6/testbench.sv
+++ Lab 6/testbench.sv
@@ -1,3 +1,4 @@
+
 module TESTBENCH ();
 
 timeunit 10ns;
@@ -8,6 +9,21 @@ logic [15:0] S;
 logic	Clk, Reset, Run, Continue;
 logic [11:0] LED;
 logic [6:0] HEX0, HEX1, HEX2, HEX3;
+
+// Remeber values of internal registers
+logic [15:0] MAR, MDR, PC, IR;
+logic [7:0] [15:0] REG; 
+logic BEN, N, Z, P;
+
+assign MAR = slc3.mainComputer.d0.MAR;
+assign MDR = slc3.mainComputer.d0.MDR;
+assign IR = slc3.mainComputer.d0.IR;
+assign PC = slc3.mainComputer.d0.PC;
+assign BEN = slc3.mainComputer.d0.BEN;
+assign N = slc3.mainComputer.d0.N;
+assign Z = slc3.mainComputer.d0.Z;
+assign P = slc3.mainComputer.d0.P;
+assign REG = slc3.mainComputer.d0.register_file.REG;
 
 // Instantiating the DUT
 // module and signal name
