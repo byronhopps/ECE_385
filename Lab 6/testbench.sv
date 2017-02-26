a2f58f9aecbf6f790d6353dea558f40426998eba
byronhopps <byron.hopps@gmail.com>
2017-02-25 20:03:04 -0600

    Restore testbench

---
+++ Lab 6/testbench.sv
@@ -0,0 +1,38 @@
+module TESTBENCH ();
+
+timeunit 10ns;
+timeprecision 1ns;
+
+// Internal logic signals
+logic [15:0] S;
+logic	Clk, Reset, Run, Continue;
+logic [11:0] LED;
+logic [6:0] HEX0, HEX1, HEX2, HEX3;
+
+// Instantiating the DUT
+// module and signal name
+slc3_toplevel slc3 (.*);
+
+// Clock toggle
+always begin : CLOCK_GENERATION
+#1 Clk = ~Clk;
+end
+
+initial begin: CLOCK_INITIALIZATION
+    Clk = 0;
+end
+
+
+// Tests start here
+initial begin : TEST_VECTORS
+    S = 16'd3;
+    Run = 1;
+//  Continue = 1;
+#4  Reset = 1;
+#4  Reset = 0;
+#4  Reset = 1;
+#4  Run = 0;
+#4  Run = 1;
+
+end
+endmodule
