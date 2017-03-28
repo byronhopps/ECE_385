9b0b117430a1ca33aa59808a2c13db3813a56439
byronhopps <byron.hopps@gmail.com>
2017-03-28 02:04:25 -0500

    Correct configuration issues with lab9_profile software project

--- Lab_9/software/lab9_profile/main.c
+++ Lab_9/software/lab9_profile/main.c
@@ -27,9 +27,8 @@
 
 int main()
 {
-    int i;
-    unsigned char plainText[33]; //should be 1 more character to account for string terminator
-    unsigned char plainKey[33];
+//    unsigned char plainText[33]; //should be 1 more character to account for string terminator
+//    unsigned char plainKey[33];
 
     // Start with pressing reset
     *to_hw_sig = 0;
@@ -39,23 +38,12 @@ int main()
 
     while (1) {
         *to_hw_sig = 0;
-        printf("\n");
-
-        printf("\nEnter plain text:\n");
-        scanf ("%s", plainText);
-        printf ("\n");
-        printf("\nEnter key:\n");
-        scanf ("%s", plainKey);
-        printf ("\n");
 
         // Convert message and key from strings to byte arrays
-        unsigned char message[16];
-        unsigned char key[16];
-
-        for (int i = 0; i < 16; i++) {
-            message[i] = charsToHex(plainText[2*i], plainText[2*i+1]);
-            key[i] = charsToHex(plainKey[2*i], plainKey[2*i+1]);
-        }
+        unsigned char message[16] = {0xEC, 0xE2, 0x98, 0xDC, 0xEC, 0xE2, 0x98, 0xDC,
+                0xEC, 0xE2, 0x98, 0xDC, 0xEC, 0xE2, 0x98, 0xDC};
+        unsigned char key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
 
         int counter = 0;
 		while (1) {
@@ -63,57 +51,11 @@ int main()
 			// Declare an array for the result and put the encrypted message there
 			unsigned char encryptedMsg[16];
 			encryptAES(message, key, encryptedMsg);
-			printf("%d", counter);
-			// Display the encrypted message
-/*			printf("\nEncrypted message is\n");
-			for (int i = 0; i < 16; i++) {
-				printf("%x", encryptedMsg[i]);
-			}
-			putchar('\n');
 
-			// Transmit encrypted message to hardware side for decryption.
-			printf("\nTransmitting message...\n");
-
-			for (i = 0; i < 16; i++) {
-				*to_hw_sig = 1;
-				*to_hw_port = encryptedMsg[i]; // encryptedMsg is your encrypted message
-				// Consider to use charToHex() if your encrypted message is a string.
-				while (*to_sw_sig != 1);
-				*to_hw_sig = 2;
-				while (*to_sw_sig != 0);
-			}
-			*to_hw_sig = 0;
-
-			// Transmit encrypted message to hardware side for decryption.
-			printf("\nTransmitting key...\n");
-
-			//TODO: Transmit key
-
-			printf("\n\n");
-
-			// Skip rest of code for week 1
-			continue;
-
-			while (*to_sw_sig != 2);
-
-			printf("\nRetrieving message...\n");
-
-			for (i = 0; i < 16; ++i) {
-				*to_hw_sig = 1;
-				while (*to_sw_sig != 1);
-				//str[i] = *to_sw_port;
-				*to_hw_sig = 2;
-				while (*to_sw_sig != 0);
-			}
-
-			printf("\n\n");
-
-			printf("Decoded message:\n");
-
-			// TODO: print decoded message
+			if (counter % 10 == 0)
+				printf("\r%6d\n", counter);
 		}
-*/
-	}
+    }
     return 0;
 }
 
